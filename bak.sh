406f616d240184716124005ac6e3a73322718202
flybird1971 <1607450455@qq.com>
2015-08-13 18:22:42 +0800

    修改备份脚本

--- bak.sh
+++ bak.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
 exec 1>>/tmp/log.log; #设置脚本所以输出 重定向到 /tmp/log.log文件
+exec 2>/dev/null;
+
+backend(){
+     #如果备份目录不存在，创建
+     if [ ! -d $2 ];then
+     	mkdir $2;
+     fi
+     
+     #备份.......
+     if [ ! -d $3 ];then
+     	echo "3 $3 is not exists ";
+     elif [ -f $1 ];then
+     		rm $1;
+     		tar -czPf $1  $3;
+     		echo "备份成功";
+     else
+     	tar -czf $1  $3;
+     	echo "备份成功";
+     fi
+}
+
+
 
 sourcePath='/datum';
 targetPath='/var/www/bak';
@@ -8,21 +30,11 @@ targetFile="$targetPath/datum.bak.tar.gz";
 
 # 写入备份日期
 date; echo "---------------------------------------------------------------------";
+backend $targetFile $targetPath $sourcePath;
 
-#如果备份目录不存在，创建
-if [ ! -d $targetPath ];then
-	mkdir $targetPath;
-fi
-
-#备份.......
-if [ ! -d $sourcePath ];then
-	echo "sourcePath $sourcePath is not exists ";
-elif [ -f $targetFile ];then
-		rm $targetFile;
-		tar -czPf $targetFile  $sourcePath;
-		echo "备份成功";
-else
-	tar -czf $targetFile  $sourcePath;
-	echo "备份成功";
-fi
+sourcePath="/var/www/blog";
+targetPath="/datum/bak";
+targetFile="$targetPath/blog.bak.tar.gz";
+backend $targetFile $targetPath $sourcePath;
 exit 0;
+
